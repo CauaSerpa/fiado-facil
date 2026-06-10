f5bdf250aaf1e844f9e96dcbcc98c1aebdb3c110
Cauã <cauaserpa092@gmail.com>
2026-06-10 14:41:23 -0300

    feat: deixa StatusBar transparente e aplica cor personalizada

--- android/app/src/main/java/br/com/cauaserpa/fiadosimples/MainActivity.java
+++ android/app/src/main/java/br/com/cauaserpa/fiadosimples/MainActivity.java
@@ -1,5 +1,28 @@
 package br.com.cauaserpa.fiadosimples;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        androidx.core.splashscreen.SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        
+        if (bridge != null && bridge.getWebView() != null) {
+            bridge.getWebView().setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        WindowInsetsControllerCompat controller = WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        controller.setAppearanceLightStatusBars(true);
+    }
+}
